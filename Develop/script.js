a8a204dca1af55fe1aad54523e9071174daa4028
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-16 17:04:27 -0400

    JavaScript, Started working on the momentJS api and able to get the hour. Next will be to grab the day of the week and the month and number date to get the api data out of the way

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,6 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // globally call moment api and the hour to know when to change the attribute
+  const now = moment(); // moment api
+  let hour = now.hours(); // getting the hour at the moment
+  console.log(hour);
+});
