65bfda857d66426fa6f44aeaf1da2ab3b7564c95
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-17 23:24:11 -0400

    JS, Added a function to let the real time hours effect the color of the time-blocks to know what time elapsed, which one is present, and which one is the future. Next trying to figure out how to update the time if possible every second or minute and then after that work on the local storage of the web app

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,24 @@ $(document).ready(function () {
 
   let currentDay = $("#currentDay");
   const divContainer = $(".container");
-  currentDay.text(moment().format("llll"));
+  function updateCurrentTime() {
+    currentDay.text(moment().format("llll"));
+  }
+
+  function givePastPresentFuture() {
+    for (let i = 0; i < 9; i++) {
+      textboxId = "#textbox-" + (i + 9);
+      let textBox = $(textboxId);
+      let textBoxHour = textBox.attr("data-hour");
+      if (hour > textBoxHour) {
+        textBox.attr("class", "past description");
+      } else if (hour == textBoxHour) {
+        textBox.attr("class", "present description");
+      } else {
+        textBox.attr("class", "future description");
+      }
+    }
+  }
 
   function displayTimeBlock() {
     for (let i = 0; i < 9; i++) {
@@ -28,10 +45,11 @@ $(document).ready(function () {
       createLabel.attr("class", "hour");
       // <textarea name="text" id="" class="description future" cols="90" rows="4"
       createTextArea.attr("name", "text");
-      createTextArea.attr("id", " ");
-      createTextArea.attr("class", "description past");
+      createTextArea.attr("id", "textbox-" + (i + 9));
+      createTextArea.attr("data-hour", i + 9);
       createTextArea.attr("cols", "90");
       createTextArea.attr("rows", "4");
+
       // <button type="submit" class="fa fa-lock saveBtn">
       createSaveBtn.attr("type", "submit");
       createSaveBtn.attr("class", "fa fa-lock saveBtn");
@@ -47,5 +65,13 @@ $(document).ready(function () {
       divContainer.append(createTimeBlockDiv);
     }
   }
+
+  function updateClock() {
+    updateCurrentTime();
+    givePastPresentFuture();
+    setTimeout(updateClock(), 1000);
+  }
+
   displayTimeBlock();
+  updateClock();
 });
